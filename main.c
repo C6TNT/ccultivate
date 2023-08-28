aff9822a5a5ab711aa93a6d279b24958e1f1c4a1
C6TNT <140625546+C6TNT@users.noreply.github.com>
2023-08-28 10:58:03 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,66 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "windows.h"
+#include "string.h"
 
-int menu()
-{
-	printf("*********************\n");
-	printf("****1.play 0.exit****\n");
-	printf("*********************\n");
-	return 0;
-}
-
-int game()
-{
-	int Rand,Guess;
-	Rand=rand()%100+1;
-	while(1)
-	{
-		printf("请输入数字>:");
-		scanf("%d",&Guess);
-		if(Guess>Rand)
-		{
-			printf("猜大了\n");	
-		}
-		else if(Guess<Rand)
-		{
-			printf("猜小了\n");
-		}
-		else
-		{
-			printf("恭喜你 猜对了！\n\n");
-			break;
-		}
-	}
-	return 0;
-}
+//int main()
+//{
+//	//shutdown -t -s 60 60s后系统将关机
+//	//system()-执行系统命令
+//	char input[20];
+//	system("shutdown -s -t 60");
+//	again :
+//	printf("请注意 你的电脑将在1分钟之后关机 输入：我是猪，即可取消关机\n请输入>:");
+//	scanf("%s",input);
+//	if(strcmp(input,"我是猪")==0)
+//	{
+//		system("shutdown -a");
+//	}
+//	else
+//	{
+//		goto again;
+//	}	
+//	return 0;
+//}
 
 int main()
 {
-	int input;
-	printf("游戏：猜数字（1-100）\n");
-	srand((unsigned int)time(NULL));
-	do{
-		menu();
-		printf("请输入>:");
-		scanf("%d",&input);
-		switch(input)
+	char input[20];
+	system("shutdown -s -t 60");
+	while(1)
+	{
+		printf("请注意 你的电脑将在1分钟之后关机 输入：我是猪，即可取消关机\n请输入>:");
+		scanf("%s",input);
+		if(strcmp(input,"我是猪")==0)
 		{
-			case 1:
-			game();
+			system("shutdown -a");
 			break;
-			
-			case 0:
-			printf("游戏结束\n");
-			break;
-			
-			default:
-			printf("输入错误\n\n");
-			break;
-		}
-		
-	}while(input);
-	
+		}		
+	}
 	
 	return 0;
 }
+
